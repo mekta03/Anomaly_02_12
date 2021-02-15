85ee2bb5b4286afabbf6e590cd3d397d29275267
mekta03 <mekta03@gmail.com>
2021-02-15 16:02:42 +1000

    Перед рефакторингом

--- auto_surfer.py
+++ auto_surfer.py
@@ -1,27 +1,38 @@
 import win32com.client 
+import pandas as pd
 
-
-def surfer_interpolation(file_csv, level):
+def surfer_interpolation(file_csv, level, name_dir):
     """
     ! Surfer должен быть запущен
     Делает интерполяцию в Surfer метод Kriging
     Выходной файл в dat формате
     """
-    x_min, x_max = 134.5, 164.5
-    y_min, y_max = 49.5, 61.5
-    spacing = 0.5
+    # x_min, x_max = 134.5, 164.5
+    # y_min, y_max = 49.5, 61.5
+    # spacing = 0.5
+    x_min, x_max = 120, 180
+    y_min, y_max = 30, 70
+    spacing = 0.01
+    
     # DataFile = r'C:\Users\malyg\Desktop\kag_64\dat\0.csv'
     DataFile = file_csv
-    
-    col_1 = 8
-    col_2 = 9
-    # col_3 = 12-19
-    
-    # for col_3 in (range(12,20) if level < 1000 else range(12,19)):
-    for col_3 in range(12,20):
-        OutFile = f"C:/Users/malyg/Desktop/kag_64/csv/grd/{level}_{col_3}"
-        app = win32com.client.gencache.EnsureDispatch('Surfer.Application')
+    # TODO Перестроить в csv до разделения NO3 в конец
+    col_1 = 2
+    col_2 = 1
+    df = pd.read_csv(file_csv, sep=',')
+    print(level)
 
+    last_col = len(df.columns)+1
+    path_project = "D:/Life/Работа/ТИНРО/Текущие проекты/Kaganovsky_2020/kag_64/"
+    # for col_3 in (range(12,20) if level < 1000 else range(12,19)):
+    for col_3 in range(3,last_col):
+        print(col_3)
+    # for col_3 in range(20,21):
+        # OutFile = rf"D:/Life/Работа/ТИНРО/Программы/Атласы (World Ocean Atlas)/test_anomaly/{name_dir}/woa_{name_dir}_{level}_{col_3}"
+        OutFile = f"C:/Users/malyg/Desktop/anomal/{name_dir}/woa_{name_dir}_{level}_{col_3}"
+        # OutFile = f"D:/Life/Работа/ТИНРО/Текущие проекты/Kaganovsky_2020/kag_64/csv/grd/{col_3}"
+        app = win32com.client.gencache.EnsureDispatch('Surfer.Application')
+        print(help(app.GridData))
         app.GridData(DataFile=DataFile,
                     # Номера колонок
                     xCol = col_1, yCol = col_2, zCol = col_3,
@@ -30,8 +41,8 @@ def surfer_interpolation(file_csv, level):
                     NumCols = ((x_max-x_min)/spacing) + 1,
                     NumRows = ((y_max-y_min)/spacing) + 1,
                     xMin = x_min, xMax = x_max, yMin = y_min, yMax = y_max, ShowReport=False,
-                    # OutFmt = win32com.client.constants.srfGridFmtXYZ,
-                    OutFmt = win32com.client.constants.srfGridFmtBinary,
+                    OutFmt = win32com.client.constants.srfGridFmtXYZ,
+                    # OutFmt = win32com.client.constants.srfGridFmtBinary,
                     OutGrid = OutFile,
                     
         )
@@ -49,102 +60,35 @@ dct_std_lvl = {**dct_1, **dct_2, **dct_3, **dct_4, **dct_5, **dct_6 }
 std_lvl = [*dct_std_lvl.keys()]
 
 
-for lvl in std_lvl:
-    file_csv = f'C:/Users/malyg/Desktop/kag_64/csv/{lvl}m.csv'
-    surfer_interpolation(file_csv, lvl)
+# for lvl in std_lvl:
+#     file_csv = f'D:/Life/Работа/ТИНРО/Текущие проекты/Kaganovsky_2020/kag_64/csv/csv/{lvl}m.csv'
+#     surfer_interpolation(file_csv, lvl)
 
-# def main(): 
-#     app = win32com.client.Dispatch("Surfer.Application") 
-#     plot = app.Documents.Add(1) 
-#     app.Visible = True 
-# main()
-
-# def main():
-#     app = win32com.client.gencache.EnsureDispatch('Surfer.Application')
-#     # app = win32com.client.Dispatch("Surfer.Application") 
-#     Plot = app.Documents.Add(1)
-#     app.Visible = True
-#     # DataFile = "C:\Program Files\Golden Software\Surfer 12\Samples\demogrid.dat"
-#     DataFile = r'C:\Users\malyg\Desktop\kag_64\dat\0.csv'
-#     # OutFile = r'C:\Users\malyg\Desktop\kag_64\dat\0_NEW_Krigin_3.grd'
-#     # app.GridData(DataFile=DataFile, xCol = 1, yCol = 2, zCol = 3, Algorithm = win32com.client.constants.srfMinCurvature, NumRows=150, NumCols=150, ShowReport=False, OutGrid= OutFile)
-#     app.GridData(DataFile=DataFile, xCol = 1, yCol = 2, zCol = 3, 
-   
-#     # DupMethod=win32com.client.constants.srfDupMedZ,
-#     # xDupTol=0.5,
-#     # yDupTol=0.5,
-    
-
-#     Algorithm = win32com.client.constants.srfKriging, 
-#     NumCols = 61,  
-#     NumRows = 41, 
-#     xMin = 139, xMax = 169, yMin = 40, yMax = 60, ShowReport=False,
-#     OutFmt = win32com.client.constants.srfGridFmtXYZ,
-#     OutGrid = r'C:\Users\malyg\Desktop\kag_64\dat\00_NEW_Krigin_3',
-#     )
-   
-        
-#     # #Creates a contour map and assigns the map frame to the variable "MapFrame"
-#     # MapFrame = Plot.Shapes.AddContourMap(GridFileName=OutFile)
-#     # #Changes the limits and scale of the map
-#     # MapFrame.SetLimits (xMin=0.5, xMax=4.5, yMin=0.5, yMax=3.5)
-#     # MapFrame.xLength=6
-#     # MapFrame.yLength=4
-#     # #Declares ContourMap as an Object and assigns the contour map to variable "ContourMap"
-#     # ContourMap = MapFrame.Overlays(1)
-    
-# main()
+# file_csv =r'D:\Life\Работа\ТИНРО\Текущие проекты\Kaganovsky_2020\kag_64\csv\csv\last_level.csv'
+# surfer_interpolation(file_csv, 'last_level')
 
 
-# def main_work():
-#     app = win32com.client.gencache.EnsureDispatch('Surfer.Application')
-#     # app = win32com.client.Dispatch("Surfer.Application") 
-#     # Plot = app.Documents.Add(1)
-#     # app.Visible = True
-#     # DataFile = "C:\Program Files\Golden Software\Surfer 12\Samples\demogrid.dat"
-#     DataFile = r'C:\Users\malyg\Desktop\kag_64\dat\0.csv'
-#     OutFile = r'C:\Users\malyg\Desktop\kag_64\dat\TEST_main_work2'
-#     # app.GridData(DataFile=DataFile, xCol = 1, yCol = 2, zCol = 3, Algorithm = win32com.client.constants.srfMinCurvature, NumRows=150, NumCols=150, ShowReport=False, OutGrid= OutFile)
-#     app.GridData(DataFile=DataFile, xCol = 1, yCol = 2, zCol = 3, 
-   
-#     # DupMethod=win32com.client.constants.srfDupMedZ,
-#     # xDupTol=0.5,
-#     # yDupTol=0.5,
-    
+name_of_parameters = ['dissolved_oxygen','nitrate', 'phosphat','silicate','salinity','temperature']
 
-#     Algorithm = win32com.client.constants.srfKriging, 
-#     NumCols = 61,  
-#     NumRows = 41, 
-#     xMin = 139, xMax = 169, yMin = 40, yMax = 60, ShowReport=False,
-#     OutFmt = win32com.client.constants.srfGridFmtXYZ,
-#     OutGrid = OutFile,
-#     )
-   
-        
-    # #Creates a contour map and assigns the map frame to the variable "MapFrame"
-    # MapFrame = Plot.Shapes.AddContourMap(GridFileName=OutFile)
-    # #Changes the limits and scale of the map
-    # MapFrame.SetLimits (xMin=0.5, xMax=4.5, yMin=0.5, yMax=3.5)
-    # MapFrame.xLength=6
-    # MapFrame.yLength=4
-    # #Declares ContourMap as an Object and assigns the contour map to variable "ContourMap"
-    # ContourMap = MapFrame.Overlays(1)
-    
-# main_work()
+dct_month = {1: 'January', 2: 'Febraury', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August',
+            9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+path_dir_new = r'D:/Life/Работа/ТИНРО/Программы/Атласы (World Ocean Atlas)/test_anomaly/'
 
 
+for name in name_of_parameters:
+    if name not in ['salinity','temperature']:
+        for lvl in dct_month.keys():
+            file_csv = rf'{path_dir_new}{name}/woa18_{name}_{lvl}.csv'
+
+            surfer_interpolation(file_csv, lvl, name)
+    else:
+        for lvl in dct_month.values():
+            name_of_month = lvl.lower()
+            file_csv = f'{path_dir_new}{name}/woa18_{name}_{name_of_month}.csv'
+            surfer_interpolation(file_csv, lvl)
+
+# file_csv =r'D:\Life\Работа\ТИНРО\Текущие проекты\Kaganovsky_2020\kag_64\csv\csv\last_level.csv'
+# surfer_interpolation(file_csv, 'last_level')
 
 
-# import glob 
-# import datetime 
-# #call this script like this: 
-# #\programs\python25\python.exe krig_data.py 
-# #get an instance of the Surfer application  
-# #Surfer = win32com.client.Dispatch('Surfer.Application') 
-# lFile = glob.glob('wl*.csv') 
-# for i in range(len(lFile)): 
-# #the min and max listed below are center coordinates while gdal_grid uses edge 
-# #coordinates 
-# sFilePrefix = lFile[i][:lFile[i].index('.')] 
-# Surfer.GridData(DataFile = lFile[i], xCol = 1, yCol = 2, zCol = 3, NumCols = 4133,  NumRows = 3017, xMin = 455355, xMax = 579315, yMin = 1954585, yMax = 2045065,  ShowReport = False, DupMethod = win32com.client.constants.srfDupAvg, OutFmt = win32com.client.constants.srfGridFmtBinary, OutGrid = '%s.grd' %sFilePrefix) 
-# print ('finished kriging %s at %s' % (lFile[i], datetime{source}))
