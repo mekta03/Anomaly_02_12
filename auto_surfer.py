fa202fa800c5a5a016009ecf9b92d85caf5ecf77
mekta03 <mekta03@gmail.com>
2021-02-15 17:25:56 +1000

    Рефакторинг

--- auto_surfer.py
+++ auto_surfer.py
@@ -1,9 +1,25 @@
-import win32com.client 
+# import win32com.client 
 import pandas as pd
 
-def surfer_interpolation(file_csv, level, name_dir):
+
+# df=pd.DataFrame(data={
+#                         'latidute':[0,1,2,3,4,5],
+#                         'longitude':[0,1,2,3,4,5],
+#                         'temp':[3,3,3,3,3,3]
+#                     }, index = [i for i in range(6)]
+#                     )
+# print(df.columns.get_loc("temp"))
+
+def surfer_interpolation(input_file, output_file, first_col, last_col=None):
     """
-    ! Surfer должен быть запущен
+    ! WARNING ! Surfer должен быть запущен \n
+    input_file - csv, из которого берутся данные для интерполяции; \n
+    output_file - название файла без расширения, в который будут записаны результаты интерполяции; \n
+
+    first_col - начало диапазона колонок для интерполяции (результат), \n
+                может быть списком из колонок, которые нужно проинтерполировать; \n
+    last_col - конец диапазона, если не указан, то до конца; \n
+
     Делает интерполяцию в Surfer метод Kriging
     Выходной файл в dat формате
     """
@@ -14,38 +30,41 @@ def surfer_interpolation(file_csv, level, name_dir):
     y_min, y_max = 30, 70
     spacing = 0.01
     
-    # DataFile = r'C:\Users\malyg\Desktop\kag_64\dat\0.csv'
-    DataFile = file_csv
-    # TODO Перестроить в csv до разделения NO3 в конец
-    col_1 = 2
-    col_2 = 1
-    df = pd.read_csv(file_csv, sep=',')
-    print(level)
+    DataFile = input_file
+    
+    df = pd.read_csv(input_file, sep=',')
+    col_1 = df.columns.get_loc("Longitude")
+    col_2 = df.columns.get_loc("Latitude")
+    print(col_1)
+    
+    start_range = first_col
+    
+    # Проверка: интерполирует отдельные колонки, или какой-то диапазон.
+    if type(start_range) != list:
+        end_range = last_col
+        range_cols = df.columns[start_range:end_range]
+    else:
+        range_cols = df.columns[start_range]
 
-    last_col = len(df.columns)+1
-    path_project = "D:/Life/Работа/ТИНРО/Текущие проекты/Kaganovsky_2020/kag_64/"
-    # for col_3 in (range(12,20) if level < 1000 else range(12,19)):
-    for col_3 in range(3,last_col):
+
+    for col_3 in range_cols:
         print(col_3)
-    # for col_3 in range(20,21):
-        # OutFile = rf"D:/Life/Работа/ТИНРО/Программы/Атласы (World Ocean Atlas)/test_anomaly/{name_dir}/woa_{name_dir}_{level}_{col_3}"
-        OutFile = f"C:/Users/malyg/Desktop/anomal/{name_dir}/woa_{name_dir}_{level}_{col_3}"
-        # OutFile = f"D:/Life/Работа/ТИНРО/Текущие проекты/Kaganovsky_2020/kag_64/csv/grd/{col_3}"
+        
+        OutFile = f"{output_file}_{col_3}"
+        
         app = win32com.client.gencache.EnsureDispatch('Surfer.Application')
-        print(help(app.GridData))
+        # print(help(app.GridData))
         app.GridData(DataFile=DataFile,
                     # Номера колонок
                     xCol = col_1, yCol = col_2, zCol = col_3,
                     Algorithm = win32com.client.constants.srfKriging,
                     # Spacing 
                     NumCols = ((x_max-x_min)/spacing) + 1,
-                    NumRows = ((y_max-y_min)/spacing) + 1,
                     xMin = x_min, xMax = x_max, yMin = y_min, yMax = y_max, ShowReport=False,
-                    OutFmt = win32com.client.constants.srfGridFmtXYZ,
-                    # OutFmt = win32com.client.constants.srfGridFmtBinary,
+                    OutFmt = win32com.client.constants.srfGridFmtXYZ, # dat
+                    # OutFmt = win32com.client.constants.srfGridFmtBinary, #grd
                     OutGrid = OutFile,
-                    
-        )
+                    )
 
 
 # Границы уровней
@@ -60,35 +79,38 @@ dct_std_lvl = {**dct_1, **dct_2, **dct_3, **dct_4, **dct_5, **dct_6 }
 std_lvl = [*dct_std_lvl.keys()]
 
 
-# for lvl in std_lvl:
-#     file_csv = f'D:/Life/Работа/ТИНРО/Текущие проекты/Kaganovsky_2020/kag_64/csv/csv/{lvl}m.csv'
-#     surfer_interpolation(file_csv, lvl)
+for lvl in std_lvl:
+    input_file = f'D:/Life/Работа/ТИНРО/Текущие проекты/Kaganovsky_2020/kag_64/csv/csv/{lvl}m.csv'
+    output_file = f'D:/Life/Работа/ТИНРО/Текущие проекты/Kaganovsky_2020/kag_64/csv/dat/{lvl}m'
+    col_range = [*(range(11, 18),19,20,18)]
+    surfer_interpolation(input_file, output_file, col_range)
 
 # file_csv =r'D:\Life\Работа\ТИНРО\Текущие проекты\Kaganovsky_2020\kag_64\csv\csv\last_level.csv'
 # surfer_interpolation(file_csv, 'last_level')
 
 
-name_of_parameters = ['dissolved_oxygen','nitrate', 'phosphat','silicate','salinity','temperature']
+# name_of_parameters = ['dissolved_oxygen','nitrate', 'phosphat','silicate','salinity','temperature']
 
-dct_month = {1: 'January', 2: 'Febraury', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August',
-            9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+# dct_month = {1: 'January', 2: 'Febraury', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August',
+#             9: 'September', 10: 'October', 11: 'November', 12: 'December'}
 
-path_dir_new = r'D:/Life/Работа/ТИНРО/Программы/Атласы (World Ocean Atlas)/test_anomaly/'
+# path_dir_new = r'D:/Life/Работа/ТИНРО/Программы/Атласы (World Ocean Atlas)/test_anomaly/'
 
 
-for name in name_of_parameters:
-    if name not in ['salinity','temperature']:
-        for lvl in dct_month.keys():
-            file_csv = rf'{path_dir_new}{name}/woa18_{name}_{lvl}.csv'
+# for name in name_of_parameters:
+#     if name not in ['salinity','temperature']:
+#         for lvl in dct_month.keys():
+#             print(lvl)
+            
+#             input_file = rf'{path_dir_new}{name}/woa18_{name}_{lvl}.csv'
+#             output_file = f'{path_dir_new}{name}/woa18_{name}_{lvl}'
+#             surfer_interpolation(input_file,output_file, lvl, name)
+#     else:
+#         for lvl in dct_month.values():
+#             name_of_month = lvl.lower()
+#             file_csv = f'{path_dir_new}{name}/woa18_{name}_{name_of_month}.csv'
+#             surfer_interpolation(file_csv, lvl)
 
-            surfer_interpolation(file_csv, lvl, name)
-    else:
-        for lvl in dct_month.values():
-            name_of_month = lvl.lower()
-            file_csv = f'{path_dir_new}{name}/woa18_{name}_{name_of_month}.csv'
-            surfer_interpolation(file_csv, lvl)
-
-# file_csv =r'D:\Life\Работа\ТИНРО\Текущие проекты\Kaganovsky_2020\kag_64\csv\csv\last_level.csv'
-# surfer_interpolation(file_csv, 'last_level')
-
+# # file_csv =r'D:\Life\Работа\ТИНРО\Текущие проекты\Kaganovsky_2020\kag_64\csv\csv\last_level.csv'
+# # surfer_interpolation(file_csv, 'last_level')
 
